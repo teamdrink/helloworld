3aabdf1af5dd50c852977419d736f96886aecb9b
teamdrink <sznejderdysk@gmail.com>
2016-03-11 19:40:48 +0100

    Update and rename helloWorld.java to helloworld.java

--- helloworld/helloworld.java
+++ helloworld/helloworld.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 
-public class HelloWorldGUI1 {
+public class helloworld {
    
    public static void main(String[] args) {
       JOptionPane.showMessageDialog( null, "Hello World!" );
