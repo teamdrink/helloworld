1de0fb1a4b8ae4aaba49e0bf2d46fc30b73ee141
div <div@Asus>
2016-03-17 13:38:55 +0100

    nanan

--- helloworld/helloworld.java
+++ helloworld/helloworld.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 public class helloworld {
    
    public static void main(String[] args) {
-      JOptionPane.showMessageDialog( null, "Siemdsaa Wszystkim!" );
+      JOptionPane.showMessageDialog( null, "Siemdsaa Wszystkim!\n nana" );
    }
 
 } //zmiana 2
